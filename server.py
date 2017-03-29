da7d6c123df6ec2007049dd99689211032708439
xlcr <akito989@hotmail.com>
2017-03-29 22:36:22 +0800

    Tree Equation Functions

--- server.py
+++ server.py
@@ -2,10 +2,10 @@ import sys
 import random
 import sqlite3
 import os
-from flask import Flask, render_template, request, jsonify, send_from_directory, redirect, url_for
+from treelib import Tree
 from genetic_algorithm import Container
 from werkzeug.utils import secure_filename
-
+from flask import Flask, render_template, request, jsonify, send_from_directory, redirect, url_for
 
 #--------------------------------------#
 app = Flask(__name__, static_url_path='')
@@ -54,6 +54,16 @@ def single():
     else:
 
         return render_template("single_model.html", selectModel=selectModel)
+
+@app.route('/treeEQ')
+def treeEQ():
+
+    equation = generateEquation(generateTree())
+    print(equation)
+    selectModel = getRow("models")
+
+    return render_template("single_model.html", selectModel=selectModel, shader=equation)
+
 
 @app.route('/view_single', methods=["POST"])
 def viewSingle():
@@ -202,7 +212,6 @@ def uploadMulti():
 
     return render_template("index.html")
 
-
 #--------------------------------------#
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
@@ -224,6 +233,146 @@ def getRow(table):
 
     return rows
 
+#=======================================#
+#          Equation from Trees          #
+#=======================================#
+
+operators = ["+", "-", "/", "*"]
+cOperators = ["sin", "cos", "tan"]
+
+def randomOperator():
+
+    operator = ["+", "-", "/", "*", "sin", "cos", "tan"]
+
+    return random.choice(operator)
+
+def randomVariable():
+
+    variables = ["x", "y", "z"]
+
+    return random.choice(variables)
+
+def randomValue():
+
+    variables = ["x", "y", "z"]
+
+    if random.randint(0, 100) % 2 == 0:
+        num = random.randint(1, 1)
+        if num == 0:
+            return randomValue()
+        return "(" + str(num) + ")"
+    else:
+        return random.choice(variables)
+
+def randomAll():
+
+    variables = ["x", "y", "z", "+", "-", "/", "*", "sin", "cos", "tan"]
+
+    if random.randint(0, 100) % 2 == 0:
+        num = random.randint(1, 1)
+        if num == 0:
+            return randomAll()
+        return "(" + str(num) + ")"
+    else:
+        return random.choice(variables)
+
+def determineNoChildren(operator):
+
+    #determine how many children the parent would have based on the operator
+
+    if operator == "+" or operator == "-" or operator == "/" or operator == "*":
+        return 2
+    else:
+        return 1
+
+def opChild(tree, index):
+
+    #genetate string of equations from index's child, based on type of operators
+
+    children = tree.is_branch(index)
+    equation = "(" + str(tree[children[0]].tag) + tree[index].tag + str(tree[children[1]].tag) + ")"
+    tree[index].tag = equation
+
+    return tree
+
+def cOpChild(tree, index):
+
+    child = tree.is_branch(index)
+    equation = str(tree[index].tag) + "(" + str(tree[child[0]].tag) + ")"
+    tree[index].tag = equation
+
+    return tree
+
+def generateTree():
+
+    tree = Tree()
+    parent = []
+    opParent = 1
+    operatorChild = 3
+
+    tree.create_node(randomOperator(), 0)
+
+    depth = determineNoChildren(tree[0].tag)
+
+    for x in range(0, depth):
+
+        if x == 0:
+            tree.create_node(randomVariable(), opParent, parent=0)
+            if tree[opParent].tag in operators or tree[opParent].tag in cOperators:
+                parent.append(opParent)
+            opParent += 1
+        else:
+            tree.create_node(randomAll(), opParent, parent=0)
+            if tree[opParent].tag in operators or tree[opParent].tag in cOperators:
+                parent.append(opParent)
+            opParent+=1
+
+    for node in parent:
+
+        depth = determineNoChildren(tree[node].tag)
+
+        for i in range(0, depth):
+
+            if i == 0:
+                tree.create_node(randomValue(), operatorChild, parent=node)
+                operatorChild += 1
+
+            else:
+                tree.create_node(randomValue(), operatorChild, parent=node)
+                operatorChild+=1
+
+    return tree
+
+def generateEquation(tree):
+
+    if tree[0].tag in cOperators:
+
+        if tree[1].tag in cOperators:
+            tree = cOpChild(tree, 1)
+            tree = cOpChild(tree, 0)
+
+        elif tree[1].tag in operators:
+            tree = opChild(tree, 1)
+            tree = cOpChild(tree, 0)
+
+        else:
+            tree[0].tag = tree[0].tag + "(" + str(tree[1].tag) + ")"
+
+    else:
+
+        children = tree.is_branch(0)
+
+        for child in children:
+
+            if tree[children[child-1]].tag in cOperators:
+                tree = cOpChild(tree, child)
+
+            elif tree[children[child-1]].tag in operators:
+                tree = opChild(tree, child)
+
+        tree = opChild(tree, 0)
+
+    return tree[0].tag
 #--------------------------------------#
 def main():
     app.run(
